99e66218b865ae863e014b03a1e4c74dc7e5aab0
Vienna Yee <viennayee@gmail.com>
2017-10-05 02:44:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  welcome = "Welcome to Tic Tac Toe!"
+  open_spaces = "   |   |   "
+  horizontal_line = "-----------"
+  puts welcome
+  2.times {puts  open_spaces, horizontal_line}
+  puts open_spaces
+end 
