342e119e1a518877dee1fe472fe00cd395b8a724
Vienna Yee <viennayee@gmail.com>
2017-10-05 02:56:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-print "Welcome to Tic Tac Toe
-     |   |
+print "     |   |
   -----------
      |   |
   -----------
